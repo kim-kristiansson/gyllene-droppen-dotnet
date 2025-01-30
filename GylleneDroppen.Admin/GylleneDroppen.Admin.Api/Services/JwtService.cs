9cedb0f228e0e9fcdc8ef4433f97d64f85167b5f
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2025-01-30 21:57:33 +0100

    Create JwtService

---
+++ GylleneDroppen.Admin/GylleneDroppen.Admin.Api/Services/JwtService.cs
@@ -0,0 +1,33 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using GylleneDroppen.Admin.Api.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GylleneDroppen.Admin.Api.Services;
+
+public class JwtService(IOptions<JwtOptions> jwtOptions)
+{
+    public string GenerateToken(Guid userId)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.Secret));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim>
+        {
+            new (ClaimTypes.Name, userId.ToString()),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: jwtOptions.Value.Issuer,
+            audience: jwtOptions.Value.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(jwtOptions.Value.ExpirationMinutes),
+            signingCredentials: credentials
+        );
+        
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
