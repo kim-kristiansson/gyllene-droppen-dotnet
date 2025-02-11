9a4d040e53920e833a494b9d77a49e39695778f5
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2025-02-12 00:54:34 +0100

    Clean up unused files

--- GylleneDroppen.Admin/GylleneDroppen.Api/Models/Location.cs
+++
@@ -1,8 +0,0 @@
-namespace GylleneDroppen.Api.Models;
-
-public class Location
-{
-    public required Guid Id { get; set; }
-    public required string Address { get; set; }
-    public required string City { get; set; }
-}
